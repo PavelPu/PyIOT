5846741f97eee9da18a1fb270dd4890ec1df1e3d
PavelPu <pustovalovpa@hotmail.com>
2017-09-26 13:11:14 +0300

    read temperature from 2 sensors

---
+++ readData.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+
+import os
+import glob
+import time
+
+base_dir = '/sys/bus/w1/devices/'
+
+#device_names = ['28-051700f1d8ff', '28-051700f63cff']
+
+#device_folders = ['/sys/bus/w1/devices/28-051700f1d8ff', '/sys/bus/w1/devices/28-051700f63cff']
+# /sys/bus/w1/devices/28-051700f63cff
+
+
+device_files = ['/sys/bus/w1/devices/28-051700f1d8ff/w1_slave', '/sys/bus/w1/devices/28-051700f63cff/w1_slave'] 
+
+ 
+def read_temp_raw(dev_file):
+    f = open(dev_file, 'r')
+    lines = f.readlines()
+    f.close()
+    return lines
+ 
+def read_temp(dev_file):
+    
+    lines = read_temp_raw(dev_file)
+    # Analyze if the last 3 characters are 'YES'.
+    while lines[0].strip()[-3:] != 'YES':
+        time.sleep(0.2)
+        lines = read_temp_raw(dev_file)
+    # Find the index of 't=' in a string.
+    equals_pos = lines[1].find('t=')
+    if equals_pos != -1:
+        # Read the temperature .
+        temp_string = lines[1][equals_pos+2:]
+        temp_c = float(temp_string) / 1000.0
+
+        return temp_c
+
+def main():
+
+
+    localtime = time.asctime( time.localtime(time.time()) )
+    print ("Local current time :", localtime)
+
+    temp = read_temp(device_files[0])
+    temp2 = read_temp(device_files[1])
+    
+    print (localtime, "\ttemp:", temp, "\ttemp2:", temp2)
+
+    #logfile = open('templog.txt', 'a')
+
+    string = localtime + ";" + str(temp) + ";" + str(temp2) + "\n"
+    print(string)
+    #
+    #logfile.write(string)
+    
+    #logfile.close()
+
+if __name__=="__main__":
+    main()
