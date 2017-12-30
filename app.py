1394732f31c58c99f9d0d0456e6ddc4dec959ad0
PavelPu <pustovalovpa@hotmail.com>
2017-12-30 15:48:19 +0300

    start m 2

--- app.py
+++ app.py
@@ -402,8 +402,13 @@ def iothub_client_sample_run():
             #reported_state = "{\"newState\":\"standBy\",\"relaysState\":{\"dining\":\"off\"}}"
             #client.send_reported_state(reported_state, len(reported_state), send_reported_state_callback, SEND_REPORTED_STATE_CONTEXT)
             reportState(relays)
-        #message = composeStartMessage
-        #client.send_event_async(message, send_confirmation_callback, MESSAGE_COUNT)
+        message = composeStartMessage
+        client.send_event_async(message, send_confirmation_callback, MESSAGE_COUNT)
+        print ( "IoTHubClient.send_event_async accepted message [%d] for transmission to IoT Hub." % MESSAGE_COUNT )
+
+        status = client.get_send_status()
+        print ( "Send status: %s" % status )
+        MESSAGE_COUNT += 1
         
         #telemetry.send_telemetry_data(parse_iot_hub_name(), EVENT_SUCCESS, "IoT hub connection is established")
         while True:
