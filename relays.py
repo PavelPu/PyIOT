8d970e2106c6befa5e076096167faea489edd17c
PavelPu <pustovalovpa@hotmail.com>
2018-04-08 14:22:10 +0300

    Changed WH relay and bath heater

--- relays.py
+++ relays.py
@@ -7,6 +7,6 @@ class Relays:
 
     def __init__(self):
         self.dining = IO.DigitalOutputDevice(17)
-        self.bath = IO.DigitalOutputDevice(27)
-        self.waterHeater = IO.DigitalOutputDevice(22)
+        self.bath = IO.DigitalOutputDevice(22)
+        self.waterHeater = IO.DigitalOutputDevice(27)
         self.diningState = self.dining.value
